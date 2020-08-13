fea61baa83b6a54b4f6d3c2bdddc4f4e1da52629
umeneri <koishimst@gmail.com>
2020-08-13 22:12:30 +0900

    fix test

--- infrastructure/repository/init.go
+++ infrastructure/repository/init.go
@@ -12,6 +12,10 @@ import (
 var dbEngine *xorm.Engine
 
 func init() {
+	dbEngine = InitDBEngine()
+}
+
+func InitDBEngine() *xorm.Engine {
 	dbName := os.Getenv("DB_NAME")
 	if dbName == "" {
 		dbName = "gin"
@@ -21,16 +25,18 @@ func init() {
 		dbURL = fmt.Sprintf("root:root@(localhost:3306)/%s?charset=utf8", dbName)
 	}
 	var err error
-	dbEngine, err = xorm.NewEngine("mysql", dbURL)
+	engine, err := xorm.NewEngine("mysql", dbURL)
 	if err != nil {
 		log.Fatal(err.Error())
 	}
-	dbEngine.ShowSQL(true)
-	dbEngine.SetMaxOpenConns(2)
-	dbEngine.SetMapper(core.GonicMapper{})
-	_, err = dbEngine.Query("select 1")
+	engine.ShowSQL(true)
+	engine.SetMaxOpenConns(2)
+	engine.SetMapper(core.GonicMapper{})
+	_, err = engine.Query("select 1")
 	if err != nil {
 		log.Fatal("error in db init: ", err)
 	}
 	log.Println("init data base ok")
+
+	return engine
 }
